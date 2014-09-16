acc44b06fdc87ecf22e370f2645b9a5ce460f7f4
Mike Barlow <snsmurf@gmail.com>
2014-09-16 20:03:20 +0100

    Added first files and basic structor

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Turn24/Documenter
+ *
+ * Index file, receives all requests and process
+ *
+ * @package  Documenter
+ * @author  Turn24 Team <info@turn24.com>
+ * @copyright  Copyright (c), Turn 24 Ltd.
+ * @license MIT
+ * @link http://github.com/Turn24/Documenter
+ * @since  Version 0.1.0
+ */
---
+++ system/config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Turn24/Documenter
+ *
+ * Config file stores all needed configuration information
+ *
+ * @package  Documenter-Config
+ * @author  Turn24 Team <info@turn24.com>
+ * @copyright  Copyright (c), Turn 24 Ltd.
+ * @license MIT
+ * @link http://github.com/Turn24/Documenter
+ * @since  Version 0.1.0
+ */
+
+$config = array(
+
+
+
+
+
+
+
+
+
+
+
+);
