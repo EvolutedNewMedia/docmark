0089e3f0668e0c18bb7224ab17f3b37dcb62f261
Mike Barlow <snsmurf@gmail.com>
2016-01-06 23:52:58 +0000

    Moving the assets over and starting to setup the templates

--- themes/fallback/page.php
+++ themes/fallback/page.php
@@ -1,1 +1,12 @@
-<?=$page?>
+<?php
+    $this->layout(
+        $theme . '::layouts/main'
+    );
+?>
+<article>
+    <div class="page-header">
+        <h1><?=$this->e($pageTitle)?></h1>
+    </div>
+
+    <?=$page?>
+</article>
